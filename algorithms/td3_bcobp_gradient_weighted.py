fixed incorrect argument passing for td3bcobp gradient weight(fixed)

algorithms/td3_bcobp_gradient_weighted.py
@@ -280,10 +280,10 @@ class TD3_BC:  # noqa
         device: str = "cpu",
     ):
         self.actor = actor
-        self.behavior_actor = behavior_actor
-        self.behavior_actor_target = copy.deepcopy(behavior_actor)
         self.actor_optimizer = actor_optimizer
+        self.behavior_actor = behavior_actor
         self.behavior_actor_optimizer = behavior_actor_optimizer
+        self.behavior_actor_target = copy.deepcopy(behavior_actor)
         self.critic_1 = critic_1
         self.critic_1_target = copy.deepcopy(critic_1)
         self.critic_1_optimizer = critic_1_optimizer
@@ -363,11 +363,11 @@ class TD3_BC:  # noqa
             log_dict["cos"] = cos.item()
 
             # Compute behavior actor loss
-            pi = self.behavior_actor(state)
-            q = self.critic_1(state, pi)
-            lmbda = self.alpha / q.abs().mean().detach()
+            behavior_pi = self.behavior_actor(state)
+            behavior_q = self.critic_1(state, behavior_pi)
+            behavior_lmbda = self.alpha / behavior_q.abs().mean().detach()
 
-            behavior_actor_loss = -lmbda * q.mean() + F.mse_loss(pi, action)
+            behavior_actor_loss = -behavior_lmbda * behavior_q.mean() + F.mse_loss(behavior_pi, action)
             log_dict["behavior_actor_loss"] = behavior_actor_loss.item()
             # Optimize the actor
             self.behavior_actor_optimizer.zero_grad()
@@ -386,7 +386,7 @@ class TD3_BC:  # noqa
             # Optimize the actor
             self.actor_optimizer.zero_grad()
             actor_loss.backward()
-            self.behavior_actor_optimizer.step()
+            self.actor_optimizer.step()
 
             # Update the frozen target models
             soft_update(self.critic_1_target, self.critic_1, self.tau)
@@ -531,22 +531,17 @@ def train(config: TrainConfig):
             eval_score = eval_scores.mean()
             normalized_eval_score = env.get_normalized_score(eval_score) * 100.0
             evaluations.append(normalized_eval_score)
-            
-            torch.save(
-                trainer.state_dict(),
-                os.path.join(config.checkpoints_path, f"checkpoint_{t}.pt"),
-            )
 
-            # evaluate 
-            eval_scores = eval_actor(
+            # evaluate behavior policy
+            behavior_eval_scores = eval_actor(
                 env,
                 behavior_actor,
                 device=config.device,
                 n_episodes=config.n_episodes,
                 seed=config.seed,
             )
-            eval_score = eval_scores.mean()
-            behavior_normalized_eval_score = env.get_normalized_score(eval_score) * 100.0
+            behavior_eval_scores = behavior_eval_scores.mean()
+            behavior_normalized_eval_score = env.get_normalized_score(behavior_eval_scores) * 100.0
 
             print("---------------------------------------")
             print(
@@ -555,6 +550,10 @@ def train(config: TrainConfig):
             )
             print("---------------------------------------")
 
+            torch.save(
+                trainer.state_dict(),
+                os.path.join(config.checkpoints_path, f"checkpoint_{t}.pt"),
+            )
 
             wandb.log(
                 {"evaluate/actor_normalized_score": normalized_eval_score,